79d3edf6dab9b978a5a93c8879650643c99ac25d
Tejaswini <tejaswini.bs7@gmail.com>
2020-11-13 16:46:04 +0100

    Ui updates

---
+++ app/src/main/java/com/example/grocerylist/AdapterGroceryList.kt
@@ -0,0 +1,5 @@
+package com.example.grocerylist
+
+class AdapterGroceryList {
+
+}
--- app/src/main/java/com/example/grocerylist/MainActivity.kt
+++ app/src/main/java/com/example/grocerylist/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.grocerylist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab_btn.setOnClickListener()
+                {
+                startActivity(Intent(this,GroceryList::class.java))
+                }
+
+    }
+    override fun onResume()
+    {
+        super.onResume()
+        loadList()
+
+    }
+
+    private fun loadList()
+    {
+        Toast.makeText(this,"List Loaded",Toast.LENGTH_SHORT).show()
     }
 }
